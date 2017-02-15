1330c18a873f6991ead265716b6c07d937c6c091
Jeffrey Bian <jeffrey@modsy.com>
2017-02-14 16:57:03 -0800

    Removed the local search path. Need to install anyway

--- airflow/bin/airflow.sh
+++ airflow/bin/airflow.sh
@@ -6,5 +6,5 @@ export BASE_LOG_FOLDER=${AIRFLOW_HOME}/logs
 export AIRFLOW_CONFIG=${AIRFLOW_HOME}/airflow.cfg
 export AWS_ACCESS_KEY_ID=AKIAJNTC4BWNEPEHTIVA
 export AWS_SECRET_ACCESS_KEY=IwzaYbsPkUVhfO%2BakZbnNVey9qBsYzx9KVeOARy%2B
-export BOTO_ENDPOINTS=${AIRFLOW_HOME}/endpoints.json
-python ./airflow "$@"
+#export BOTO_ENDPOINTS=${AIRFLOW_HOME}/endpoints.json
+airflow "$@"
